a1e1fa4817ca880445a0ea366223716632767657
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-08-17 17:05:55 +0900

    first commit

---
+++ section2/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -0,0 +1,14 @@
+package com.eazybytes.springsecsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan("com.eazybytes.springsecsection1.controller")
+public class EazyBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBankBackendApplication.class, args);
+	}
+
+}
