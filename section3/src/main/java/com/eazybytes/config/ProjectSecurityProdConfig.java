31a6c0e8b42c66535749cb6b58130cdbaad75bce
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-08-31 23:58:23 +0900

    update

--- section3/src/main/java/com/eazybytes/config/ProjectSecurityProdConfig.java
+++ section3/src/main/java/com/eazybytes/config/ProjectSecurityProdConfig.java
@@ -11,8 +11,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
-@Profile("!prod")
-public class ProjectSecurityConfig {
+@Profile("prod")
+public class ProjectSecurityProdConfig {
 
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
